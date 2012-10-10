8afe5eca1836f54733eec6b2c72f693d88240443
Krishna Kamath <krishna.kamath@gmail.com>
2012-10-09 20:59:36 -0500

    modified code

--- convert_to_json.py
+++ convert_to_json.py
@@ -33,9 +33,6 @@ def convert_file_to_json(input_file, output_file):
 if __name__ == '__main__':
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
-#    convert_file_to_json(input_file, output_file)
-#    input_dir = '/Users/kykamath/Documents/workspace_sept_2012/infolab_hackathon/android_enthusiasts'
-#    output_dir = '/Users/kykamath/Documents/workspace_sept_2012/infolab_hackathon/android_enthusiasts_json'
     for file in os.listdir(input_dir):
         input_file = input_dir+'/%s'%file
         output_file = output_dir+'/%s'%file
