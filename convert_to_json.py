4c770a274cbaea1f29556ec59c71dbf4eb570577
Krishna Kamath <krishna.kamath@gmail.com>
2012-10-12 19:23:37 -0500

    modified files

--- convert_to_json.py
+++ convert_to_json.py
@@ -3,40 +3,14 @@ Created on Oct 9, 2012
 
 @author: kykamath
 '''
-#! /opt/local/bin/python
 from library.classes import GeneralMethods
 from library.file_io import FileIO
 from xml.etree import ElementTree
 import os
-import re
 import sys
 
-def convert_row_to_dict(row):
-    #try:
-        for tag in ['Location', 'Text', 'Comment', 'UserDisplayName', 'AboutMe', 'DisplayName', 'Title']:
-            row = re.sub('%s="( )*'%tag, '%s="'%tag, row)
-        data = {}
-        for c in row[5:-2].split('" '):
-            if c:
-                c+='"'
-                if c[-2]=='=': c = c[:-2]+'= "'
-                print c
-                key, value = c.split('="')
-                value=value[:-1]
-                data[key] = value
-        if data: return data
-    #except Exception as e:
-        #print 'Caught expection', e
-        #pass
-    
-def convert_file_to_json(input_file, output_file):
-    output_file=output_file[:-3]+'json'
-    for line in FileIO.iterateLinesFromFile(input_file):
-        if line[:4]=='<row': 
-            data = convert_row_to_dict(line)
-            if data: FileIO.writeToFileAsJson(data, output_file)
-
 def convert_file_to_json_using_xml(input_file, output_file):
+    output_file=output_file[:-3]+'json'
     rows = ElementTree.iterparse(input_file)
     for event, row in rows:
         if row.tag=='row':
@@ -55,7 +29,5 @@ if __name__ == '__main__':
     for file in os.listdir(input_dir):
         input_file = input_dir+'/%s'%file
         output_file = output_dir+'/%s'%file
-#        if is_xml(input_file): convert_file_to_json(input_file, output_file)
         if is_xml(input_file): convert_file_to_json_using_xml(input_file, output_file)
         else: copy_file(input_file, output_file)
-        
